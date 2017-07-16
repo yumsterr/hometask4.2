33d44617c7af03a601359b92dd875edcd857d069
Lina Vasylieva <Yumster@Yumster-mac.local>
2017-07-16 16:00:56 +0300

    index set 403

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.send('ok');
+    let err = new Error('Forbidden');
+    err.status = 403;
+    next(err);
 });
 
 module.exports = router;
